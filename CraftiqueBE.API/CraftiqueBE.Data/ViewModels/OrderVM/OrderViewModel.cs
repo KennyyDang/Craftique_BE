51d701084242b7ff4968ccdbf63e1d21c835f32c
Admin <dangtrungkien748@gmail.com>
2025-07-11 01:57:04 +0700

    Update code về CustomProduct

--- CraftiqueBE.API/CraftiqueBE.Data/ViewModels/OrderVM/OrderViewModel.cs
+++ CraftiqueBE.API/CraftiqueBE.Data/ViewModels/OrderVM/OrderViewModel.cs
@@ -28,5 +28,6 @@ namespace CraftiqueBE.Data.ViewModels.OrderVM
 		public int ProductItemID { get; set; }
 		public int Quantity { get; set; }
 		public double Price { get; set; }
+		public string? CustomProductFileID { get; set; }
 	}
 }
